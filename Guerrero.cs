b787be53e98aa2a9a24ed2ca3aea12e6d972d28a
entrenadorDeFocas <34198886+LittleWatson21@users.noreply.github.com>
2019-03-14 09:52:55 -0600

    Update Guerrero.cs

--- Guerrero.cs
+++ Guerrero.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Juego_Guerra
 {
-    class Guerrero
+    abstract class Guerrero : SerVivo
     {
+        private string _Nombre;
+        protected int _Blanco; // indicara la posición del guerrero.
+        private static Random ran = new Random();
+
+        public Guerrero (string nombre)
+        {
+            _Nombre = nombre;
+            _Blanco = ran.Next(10);
+        }
+
+        public string  Nombre
+        {
+            get { return _Nombre; }
+        }
+        public int Disparar ()
+        {
+            if (_Vivo)
+                return ran.Next(10);
+            else return -1;
+        }
+        public override string ToString()
+        {
+            return _Nombre +  base.ToString();
+        }
     }
 }
